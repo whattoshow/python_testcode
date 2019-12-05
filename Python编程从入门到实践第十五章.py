2a67081efa895804f5f581a6008cafef5619e8ff
whattoshow <461905672@qq.com>
2019-12-05 16:14:37 +0800

    如何使用matplotlib绘制图标

---
+++ Python编程从入门到实践第十五章.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import matplotlib.pyplot as plt
+squares = [1,4,9,16,25]
+plt.plot(squares,linewidth=5)
+'''设置图标标题，并给坐标轴加上标签'''
+plt.title("Square numbers",fontsize=24)
+plt.xlabel("Value",fontsize=14)
+plt.ylabel("Square of value",fontsize=14)
+
+'''设置刻度标记的大小'''
+plt.tick_params(axis='both',lablesize=14)
+plt.show()
