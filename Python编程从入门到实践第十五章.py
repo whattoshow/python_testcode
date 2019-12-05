5346d91cd70242356219ccfdc30c607400c27891
whattoshow <461905672@qq.com>
2019-12-05 17:47:23 +0800

    添加程序

--- Python编程从入门到实践第十五章.py
+++ Python编程从入门到实践第十五章.py
@@ -4,10 +4,11 @@ Spyder Editor
 
 This is a temporary script file.
 """
-
+#使用plot绘制折线图
 import matplotlib.pyplot as plt
-squares = [1,4,9,16,25]
-plt.plot(squares,linewidth=5)
+input_values=[1,2,3,4,5]#x轴的值
+squares = [1,4,9,16,25]#y轴的值
+plt.plot(input_values,squares,linewidth=3)
 '''设置图标标题，并给坐标轴加上标签'''
 plt.title("Square numbers",fontsize=24)
 plt.xlabel("Value",fontsize=14)
@@ -16,3 +17,5 @@ plt.ylabel("Square of value",fontsize=14)
 '''设置刻度标记的大小'''
 plt.tick_params(axis='both',lablesize=14)
 plt.show()
+
+
